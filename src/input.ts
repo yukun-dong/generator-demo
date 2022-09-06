1dd9e7b044d54353ae643057fc1eeee65c85ef1a
yukun-dong <dongyukun1995@163.com>
2022-09-06 10:11:51 +0800

    add cd.yml

--- src/input.ts
+++ src/input.ts
@@ -1,6 +1,8 @@
 interface Input{
     type: 'project'| 'sample'| 'block';
     url: string;
+    appScenario: string;
+    language: string;
     projectName?: string;
     sampleName?: string;
     folder: string;
